75fbe46ca6f5287ecc8632a510b7692f6a174857
PoliukhovychAnna <116736506+PoliukhovychAnna@users.noreply.github.com>
2023-04-05 14:26:34 +0300

    solved task-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,53 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const list = document.querySelector(".gallery");
+
+function createGallery(arr) {
+   return arr.map(
+        ({ preview, original, description }) =>
+            `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`).join('')
+};
+
+list.insertAdjacentHTML("beforeend", createGallery(galleryItems));
+
+list.addEventListener("click", onImgClick)
+
+function onImgClick(evt) {
+    evt.preventDefault();
+
+    const img = evt.target
+    if (!img.classList.contains("gallery__image")) {
+        return
+    }
+
+    const imgSrc = img.dataset.source;
+
+    const instance = basicLightbox.create(
+        `<img src="${imgSrc}" width="800" height="600">`);
+    instance.show();
+
+    const visible = instance.visible();
+    
+    document.addEventListener("keydown", (e) => {
+        if (e.key !== "Escape") {
+            return;
+        } else {
+            if (visible) {
+                instance.close();
+            }
+        }
+    })
+}
+   
+
+
